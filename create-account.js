a357d5a6adfc60ebd4af634aabbf4cf6080202e2
Henry <henrywlam+github@gmail.com>
2023-03-17 20:55:01 -0400

    completed create-account and guessing-game functions

--- create-account.js
+++ create-account.js
@@ -1,5 +1,36 @@
-function createAccount(pin, amount) {
-
+function createAccount(newPin, newAmount = 0) {
+    let pin = newPin;
+    let amount = newAmount;
+    return {
+        checkBalance: (input)=>{
+            if(input === pin){
+                return `$${amount}`
+            }
+            return "Invalid PIN."
+        },
+        deposit: (pinInput, money)=>{
+            if(pinInput === pin){
+                amount += money;
+                return `Succesfully deposited $${money}. Current balance: $${amount}.`
+            }
+            return "Invalid PIN."
+        },
+        withdraw: (pinInput, money)=>{
+            if(pinInput === pin){
+                if(money > amount) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+                amount -= money;
+                return `Succesfully withdrew $${money}. Current balance: $${amount}.`
+            }
+            return "Invalid PIN."
+        },
+        changePin: (pinInput, newPin)=>{
+            if(pinInput === pin){
+                pin = newPin;
+                return "PIN successfully changed!"
+            }
+            return "Invalid PIN."
+        },
+    }
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
-
+    let num = Math.ceil(Math.random() * 99);
+    let guesses = 0;
+    let gameWon = false;
+    return (guess) => {
+        if(guess === num){
+            if(gameWon) return 'The game is over, you already won!';
+            gameWon = true;
+            guesses++;
+            return `You win! You found ${num} in ${guesses} guesses.`
+        } else if(guess > num) {
+            guesses++;
+            return `${guess} is too high!`
+        } else if(guess<num){
+            guesses++;
+            return `${guess} is too low!`
+        }
+    }
 }
 
 module.exports = { guessingGame };
