d0d7ecb37b02d1ec34b62d7af5845d750311ba36
Henry <henrywlam+github@gmail.com>
2023-03-17 21:10:28 -0400

    completed curriedadd function

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,13 @@
-function curriedAdd(total) {
-
+function curriedAdd(num) {
+    let total = 0;
+    if(num === undefined) return total;
+    total += num;
+    const fn = (num)=>{
+        if(num === undefined) return total;
+        total += num;
+        return fn
+    }
+    return fn
 }
 
 module.exports = { curriedAdd };
